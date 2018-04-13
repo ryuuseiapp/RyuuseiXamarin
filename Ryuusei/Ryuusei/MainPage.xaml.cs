1fc000b2afaf9fc3b64d2b42cb27d7f7f09d7184
SoraShiro <sora95shiro@gmail.com>
2018-04-14 01:25:25 +0800

    Init

---
+++ Ryuusei/Ryuusei/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Naxam.Controls.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ryuusei
+{
+    class MainPage : BottomTabbedPage
+    {
+        public MainPage()
+        {
+            this.Title = "TabbedPage";
+
+            this.ItemsSource = new NamedColor[] {
+                new NamedColor ("远虑", Color.Red),
+                new NamedColor ("近忧", Color.Yellow),
+                new NamedColor ("焦虑一下", Color.Green),
+            };
+
+            this.ItemTemplate = new DataTemplate(() => {
+                return new NamedColorPage();
+            });
+        }
+    }
+
+    // Data type:
+    class NamedColor
+    {
+        public NamedColor(string name, Color color)
+        {
+            this.Name = name;
+            this.Color = color;
+        }
+
+        public string Name { private set; get; }
+
+        public Color Color { private set; get; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    // Format page
+    class NamedColorPage : ContentPage
+    {
+        public NamedColorPage()
+        {
+            // This binding is necessary to label the tabs in
+            // the TabbedPage.
+            this.SetBinding(ContentPage.TitleProperty, "Name");
+
+            var layout = new StackLayout { Padding = new Thickness(5, 10) };
+            this.Content = layout;
+            var label = new Label { FontSize = 20 };
+            var s = new FormattedString();
+            s.Spans.Add(new Span { Text = "Check", FontAttributes = FontAttributes.Bold });
+            label.FormattedText = s;
+            layout.Children.Add(label);
+            
+        }
+    }
+}
